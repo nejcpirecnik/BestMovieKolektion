2060a37314dd49e5dcdc95cccec9796d6a6123cd
Hojnik15 <64775093+Hojnik15@users.noreply.github.com>
2022-05-06 17:14:12 +0200

    mailer-se-ne-dokoncan-in-iframe

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,11 +34,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "heroku.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'bestmoviekolektion.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode.
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
